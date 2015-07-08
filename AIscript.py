198ff7127835ab7f8e1db4d1aac8e3620e296c3d
channeng <shannon@action.io>
2015-07-07 23:41:00 -0700

    updated game with replay function

--- AIscript.py
+++ AIscript.py
@@ -8,35 +8,34 @@ board = [r1,r2,r3]
 
 # PRINTING BOARD & TURNS
 
-def printBoard():
+def printBoard(board):
 	print "\n"
 	for i in board:
 		for n in i:
 			print n,
 		print "\n"
 
-def Pturn(n):
+def Pturn(n,board):
 	
 	print "Player " + str(n) + " :"
-	
-	prow = int(raw_input("Which row? (1/2/3) \n >  ")) -1
+	prow = check123(raw_input("Which row? (1/2/3) \n >  ")) -1
 	while (prow +1) <= 0 or (prow+1) > 3:
 		print "Please re-enter row:"
-		prow = int(raw_input("Which row? (1/2/3) \n >  ")) -1
+		prow = check123(raw_input("Which row? (1/2/3) \n >  ")) -1
 
-	pcol = int(raw_input("Which col? (1/2/3) \n >  ")) -1
+	pcol = check123(raw_input("Which col? (1/2/3) \n >  ")) -1
 	while (pcol+1) <= 0 or (pcol+1) > 3:
 		print "Please re-enter col:"
-		pcol = int(raw_input("Which col? (1/2/3) \n >  ")) -1
+		pcol = check123(raw_input("Which col? (1/2/3) \n >  ")) -1
 
 	if board[prow][pcol] == 0:
 		board[prow][pcol] = n
-		printBoard()
+		printBoard(board)
 #		return rowCrit()
 	else:
-		printBoard()
+		printBoard(board)
 		print "Sorry, it's taken. Try again!"
-		Pturn(n)
+		Pturn(n,board)
 #		return False
 
 # def P2turn():
@@ -53,7 +52,7 @@ def Pturn(n):
 # 		P2turn()
 
 # GAME OVER CRITERIA
-def endCrit():
+def endCrit(board):
 	for i in board:
 		for n in i:
 			if n == 0:
@@ -80,7 +79,7 @@ def nrowmatch(nrow):
 #	return True 
 
 
-def rowCrit():
+def rowCrit(board):
 	for v in board:
 		if nrowmatch(v):
 			return True
@@ -88,7 +87,7 @@ def rowCrit():
 	else: 
 		return False
 
-def ncolmatch(ncol):
+def ncolmatch(ncol,board):
 	x = 0
 	y = 1
 	for i in range(3):
@@ -99,15 +98,15 @@ def ncolmatch(ncol):
 	else:
 		return False
 
-def colCrit():
+def colCrit(board):
 	for ncol in range(len(board[0])):
-		if ncolmatch(ncol):
+		if ncolmatch(ncol,board):
 			return True
 			break
 	else: 
 		return False
 
-def leftdiagCrit():
+def leftdiagCrit(board):
 	x = 0
 	y = 1
 	for i in range(3):
@@ -118,7 +117,7 @@ def leftdiagCrit():
 	else:
 		return False
 
-def rightdiagCrit():
+def rightdiagCrit(board):
 	x = 0
 	y = 1
 	for i in range(3):
@@ -129,71 +128,96 @@ def rightdiagCrit():
 	else:
 		return False
 
-def winCrit():
-	return rightdiagCrit() or leftdiagCrit() or colCrit() or rowCrit()
+def winCrit(board):
+	return rightdiagCrit(board) or leftdiagCrit(board) or colCrit(board) or rowCrit(board)
 
 #GAME RUNS
 
+def clearboard(board):
+	r1 = [0,0,0]
+	r2 = [0,0,0]
+	r3 = [0,0,0]
+	board = [r1,r2,r3]
+	return board
 
-def replay():
-	answer = raw_input("Do you want to play again? (y/n) \n > ")
-	if answer == "y":
-		for row in board:
-			for i in range(3):
-				row[i] == 0
-		start_game()
+def checkyn(inputt):
+	if inputt == "y" or inputt == "n":
+		return inputt
 	else:
+		inputt2 = str(raw_input("Please enter 'y' or 'n'.\n > ")).lower()
+		return checkyn(inputt2)
+
+def check123(inputt):
+	if inputt == "1" or inputt == "2" or inputt == "3":
+		return int(inputt)
+	else:
+		inputt2 = raw_input("Please enter '1' or '2' or '3'.\n > ")
+		return check123(inputt2)
+
+def replay(board):
+	inputt = str(raw_input("Do you want to play again? (y/n) \n > ")).lower()
+	answer = checkyn(inputt)
+	if answer == "y":
+		start_game(clearboard(board))
+	else:
+		print "Thanks for playing! \n ---------- Credits: Shannon Chan ----------"
 		return True
 
-def start_game():
-	printBoard()
-	who_starts = raw_input("Do you want to begin first? (y/n)")
+def start_game(board):
+	printBoard(board)
+	inputt = raw_input("Do you want to begin first? (y/n)")
+	who_starts = checkyn(inputt)
 	if who_starts == "y":
-		while not winCrit():
-			Pturn(1)
-			if winCrit():
+		while not winCrit(board):
+			Pturn(1,board)
+			if winCrit(board):
 				print "GAME OVER!!! You win!"
-				if replay():
-					break
-			elif endCrit():
+				if replay(board):
+					return
+			elif endCrit(board):
 				print "GAME OVER!!! It's a draw!"
-				if replay():
-					break
-			ai = aiprog.AIprog(2,board)
-			board[ai[0]][ai[1]] = 2
-			print "AI played row %s , col %s" %((ai[0]+1),(ai[1]+1))
-			printBoard()
-			if endCrit():
-				print "GAME OVER!!! It's a draw!"
-				if replay():
-					break
+				if replay(board):
+					return
+			else:	
+				ai = aiprog.AIprog(2,board)
+				board[ai[0]][ai[1]] = 2
+				print "AI played row %s , col %s" %((ai[0]+1),(ai[1]+1))
+				printBoard(board)
+				if endCrit(board):
+					print "GAME OVER!!! It's a draw!"
+					if replay(board):
+						return
 		else:
 			print "GAME OVER!!! AI wins"
+			if replay(board):
+					return
 
 	if who_starts == "n":
-		while not winCrit():
+		while not winCrit(board):
 			ai = aiprog.AIprog(1,board)
 			board[ai[0]][ai[1]] = 1
 			print "AI played row %s , col %s" %((ai[0]+1),(ai[1]+1))
-			printBoard()
-			if winCrit():
+			printBoard(board)
+			if winCrit(board):
 				print "GAME OVER!!! AI wins"
-				if replay():
-					break
-			elif endCrit():
+				if replay(board):
+					return           # Bug: Break here but while - else statement still runs?
+			elif endCrit(board):
 				print "GAME OVER!!! It's a draw!"
-				if replay():
-					break
-			Pturn(2)
-			if endCrit():
-				print "GAME OVER!!! It's a draw!"
-				if replay():
-					break
+				if replay(board):
+					return
+			else:
+				Pturn(2,board)
+				if endCrit(board):
+					print "GAME OVER!!! It's a draw!"
+					if replay(board):
+						return
 		else:
-			print "GAME OVER!!! You win!"
+			print "GAME OVER!!! You win!"    # Runs despite break
+			if replay(board):
+					return
 
-start_game()
-
+start_game(board)
 #win = ""
 #while True:
 #	if P1turn():
